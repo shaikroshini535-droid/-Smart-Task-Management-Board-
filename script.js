0ffa53e9caab18b13dedc31403c8b51a0483e614
sriloukikapakala <sriloukikapakal@gmail.com>
2026-06-17 11:34:45 +0530

    Added drag and drop functionality

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Smart Task Management Board");
+
+// Drag Start
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+// Allow Drop
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+// Drop
+function drop(event) {
+    event.preventDefault();
+
+    let taskId = event.dataTransfer.getData("text");
+    let task = document.getElementById(taskId);
+
+    event.target.appendChild(task);
+}
